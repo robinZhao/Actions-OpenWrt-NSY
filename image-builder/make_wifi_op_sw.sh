898f94a14669a3afa43fc34bd20c0af436d41d5f
Your Name <you@example.com>
2026-01-21 16:36:07 +0800

    use hw ssl

--- image-builder/make_wifi_op_sw.sh
+++ image-builder/make_wifi_op_sw.sh
@@ -4,11 +4,12 @@ kmod-cfg80211 kmod-mac80211 kmod-mt76-connac kmod-mt76-core kmod-mt7915e kmod-mt
 iwinfo wireless-tools wireless-regdb \
 kmod-button-hotplug kmod-leds-gpio \
 kmod-fs-vfat kmod-fs-ntfs3 kmod-fs-ext4 kmod-fs-exfat \
-luci luci-ssl luci-compat luci-i18n-base-zh-cn \
+luci luci-ssl-openssl luci-compat luci-i18n-base-zh-cn \
 luci-i18n-firewall-zh-cn luci-app-samba4 luci-i18n-samba4-zh-cn luci-app-upnp \
 luci-i18n-upnp-zh-cn luci-app-https-dns-proxy luci-i18n-https-dns-proxy-zh-cn \
 luci-app-adblock luci-i18n-adblock-zh-cn luci-app-wol luci-i18n-wol-zh-cn kmod-tun \
 zerotier tcpdump block-mount ca-certificates \
+kmod-crypto-hw-rockchip libopenssl-afalg openssl-util \
 kmod-oaf luci-app-oaf luci-i18n-oaf-zh-cn openssh-sftp-server -libustream-mbedtls libustream-openssl20201210 -apk-mbedtls apk-openssl \
 " \
 FILES=sw \
